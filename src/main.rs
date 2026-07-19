218975958e593583a797826f39abdc56f25aa663
Raynor Elgie <elgieraynor@gmail.com>
2026-07-19 22:08:36 +0000

    timing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::collections::{HashMap, HashSet};
 
+use std::time::Instant;
+
 use ferrotorch::{
     distributions::{Categorical, Distribution},
     from_vec,
@@ -68,22 +70,34 @@ fn do_gpt2(_in_str: &str) -> FerrotorchResult<()> {
 
     let mut optimizer = AdamW::new(
         gpt.deduped_params(),
-        AdamWConfig::default().with_lr(3e-4).with_betas((0.9, 0.95)),
+        AdamWConfig::default().with_lr(3e-4).with_betas((0.9, 0.95)).with_foreach(true),
     );
 
     // Do the overfit
     for i in 0..50 {
+        let start = Instant::now();
+
         let (x, y) = data_loader.next_batch::<f32>()?;
         optimizer.zero_grad();
 
         let logits = gpt.forward(&x)?;
         let loss = cross_entropy_w_gpu(&logits, &y)?;
 
-        println!("step: {i}, loss: {:?}", loss.data_vec()?);
+
 
         backward(&loss)?;
         let _total_norm = clip_grad_norm_(&gpt.parameters(), 1.0, 2.0)?;
         optimizer.step();
+
+        if let Some(bck) = ferrotorch_core::gpu_dispatch::gpu_backend() {
+            bck.synchronize(0)?; // Sync for timing
+        }
+
+        let duration = start.elapsed();
+        let tok_s = (b * t) as f64 / duration.as_secs_f64();
+
+
+        println!("step: {i}, loss: {:?}, duration: {:?}, tok_s: {:?}", loss.data_vec()?, duration, tok_s);
     }
 
     Ok(())
